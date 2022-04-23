2597e9ddf963c0ca5e0715d1259b354626fd1476
Xperis Admin <xperis.heilab@gmail.com>
2022-04-23 19:52:20 +0100

    first commit

---
+++ precache-manifest.a13664aae227448a541b676ff23981e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2099e4b91e5984b247da",
+    "url": "/static/css/main.8375455d.chunk.css"
+  },
+  {
+    "revision": "2099e4b91e5984b247da",
+    "url": "/static/js/main.d3d216f1.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "4c4e45c21b9d5021aa11",
+    "url": "/static/js/2.aecb81c7.chunk.js"
+  },
+  {
+    "revision": "380d07f833155f5dc234e637adf7abe4",
+    "url": "/index.html"
+  }
+];
